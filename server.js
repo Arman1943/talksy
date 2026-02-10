dee52e0a6e27a476fb3b24ee243af5b559594f65
Arman1943 <khindarman234@gmail.com>
2026-02-10 23:51:15 +0530

    lowered backup time

--- server.js
+++ server.js
@@ -238,4 +238,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Server running on port", PORT));
 setInterval(() => {
   runBackup().catch(console.error);
-}, 5 * 60 * 1000); // every 5 minutes
+}, 30 * 1000); // every 5 minutes
